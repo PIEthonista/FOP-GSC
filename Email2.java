9c96e765b2a1ab6fb5a475984d1b18df87eaa2c0
PIEthonista <gohyixian456@gmail.com>
2021-12-13 11:37:24 +0800

    mail

--- Email2.java
+++ Email2.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package email2;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
