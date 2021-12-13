133411f38c208b8773b856863957df8c3e099582
Jia Yu Lim <U2102820@siswa.um.edu.my>
2021-12-13 11:24:34 +0800

    Added email sent function

---
+++ Email2.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package email2;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.Authenticator;
+import javax.mail.internet.AddressException;
+public class Email2 {
+
+    public static void main(String recepient) throws Exception {
+        System.out.println("Preparing to send email");
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.smtp.host","smtp.gmail.com");
+        properties.put("mail.smtp.port","587");
+        
+        String myAccountEmail = "U2102820@siswa.um.edu.my";
+        String password = "Jiayu_02";
+        
+        Session session = Session.getInstance(properties, new Authenticator(){
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(myAccountEmail,password);
+            }
+        });
+    
+        Message message = prepareMessage(session, myAccountEmail, recepient);
+        
+        Transport.send(message);
+        System.out.println("Message sent successfully");
+    }
+    
+    private static Message prepareMessage(Session session, String myAccountEmail, String recepient){
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject("Hacker message alert!");
+            message.setText("This is the message");
+            return message;
+        } catch (Exception ex) {
+            Logger.getLogger(Email2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+            
+}
+    
+}
---
+++ Email3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package email2;
+
+/**
+ *
+ * @author asus
+ */
+public class Email3 {
+    public static void main(String[] args) throws Exception{
+        Email2.main("lcklimchenkang2002@gmail.com");
+}
+}
