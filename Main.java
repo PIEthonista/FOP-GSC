da46028785a5abe42760edff7ebf608cdcf0d79d
RONYLAU123 <ronylau123@gmail.com>
2022-01-07 21:25:30 +0800

    added book page

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main{
     public static void main(String [] args){
         CoverPage coverpage = new CoverPage();
         System.out.println(FP.getPath("GSC.png"));
+        System.out.println();
     }
 }
